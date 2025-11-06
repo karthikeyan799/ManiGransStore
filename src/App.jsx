fc7ce2d1e86c35d8fd455b1bdafd7757de0733cc
karthikeyan799 <akarthikeyan799@gmail.com>
2025-11-06 13:04:25 +0530

    600 without fast

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
       {/* <Category/> */}
       <LuxuryCollection />
       <ModernCollection />
-      <FastSelling />
+      {/* <FastSelling /> */}
       {/* <SpecialOfferse /> */}
       <Footer />
     </div>
