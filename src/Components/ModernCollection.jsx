0eb5b1719b9454a79bdef1144c464a36ff6b22a6
karthikeyan799 <akarthikeyan799@gmail.com>
2025-11-11 19:10:07 +0530

    learnmore

--- src/Components/ModernCollection.jsx
+++ src/Components/ModernCollection.jsx
@@ -56,7 +56,7 @@ export default function ModernCollection() {
                         </div>
                     </div>
                     <h5 className='text-warning'>BEST PRICE: <i className='bi bi-currency-rupee '></i>195.00</h5>
-                    <div className='modernButtons d-flex gap-2 flex-wrap justify-content-center'>
+                    <div className='modernButtons d-flex gap-2 flex-wrap justify-content-center justify-content-sm-start'>
                         <button className='learnMoreDiscount bg-white text-black'>LEARN MORE</button>
                         <button className='learnMoreDiscount mt bg-dark'>ADD TO CART</button>
                     </div>
